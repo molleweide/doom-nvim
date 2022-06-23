61c917f923fc5b4febc47e5013dcdff93d72b2ad
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-06-23 02:56:02 -0400

    prepare for moving all dui funcs to utils/tree

--- lua/doom/modules/features/dui/make_results.lua
+++ lua/doom/modules/features/dui/make_results.lua
@@ -1,5 +1,6 @@
 local ax =  require("doom.modules.features.dui.actions")
 local ut =  require("doom.modules.features.dui.utils")
+local tree = require("doom.utils.tree")
 
 local function i(x)
   print(vim.inspect(x))
@@ -308,6 +309,60 @@ end
 -- SETTINGS (USER/MODULE)
 --
 
+-- tree.traverse_table {
+--   tree = doom.settings,
+--   type = "settings",
+--   leaf = function(stack, k, v)
+--       -- collect table_path back to setting in original table
+--       local pc
+--       if #stack > 0 then
+--         pc = vim.deepcopy(stack)
+--         table.insert(pc, k)
+--       else
+--         pc = { k }
+--       end
+--       -- REFACTOR: concat table_path
+--       -- format each setting
+--       local pc_display = table.concat(pc, ".")
+--       local v_display
+--       if type(v) == "table" then
+--         local str = ""
+--         for _, x in pairs(v) do
+--           if type(x) == "table" then
+--             str = str .. ", " .. "subt"
+--           else
+--             str = str .. ", " .. x
+--           end
+--         end
+--         v_display = str -- table.concat(v, ", ")
+--       else
+--         v_display = tostring(v)
+--       end
+--       return {
+--         type = "module_setting",
+--         data = {
+--           table_path = pc,
+--           table_value = v,
+--         },
+--         list_display_props = {
+--           {"SETTING"},
+--           {pc_display},
+--           {v_display}
+--         },
+--         ordinal = pc_display,
+--         mappings = {
+--           ["<CR>"] = function(fuzzy,line, cb)
+--             i(fuzzy)
+--             -- doom_ui_state.query = {
+--             --   type = "settings",
+--             -- }
+--             -- doom_ui_state.next()
+--   		    end
+--         }
+--       }
+--   end
+-- }
+
 M.settings_flattened = function(t_settings, flattened, stack)
   local flattened = flattened or {}
   local stack = stack or {}
@@ -389,6 +444,42 @@ end
 -- PACKAGES
 --
 
+-- tree.traverse_table {
+--   tree = doom.settings,
+--   type = "settings",
+--   leaf = function(stack, k, v)
+--     local spec = v
+--     if type(k) == "number" then
+--       if type(v) == "string" then
+--         spec = { v }
+--       end
+--     end
+--     local repo_name, pkg_name = spec[1]:match("(.*)/([%w%-%.%_]*)$")
+--     return {
+--       type = "module_package",
+--       data = {
+--         table_path = k,
+--         spec = spec,
+--       },
+--       list_display_props = {
+--         {"PKG"},
+--         {repo_name},
+--         {pkg_name}
+--       },
+--       ordinal = repo_name .. pkg_name,
+--       mappings = {
+--         ["<CR>"] = function(fuzzy,line, cb)
+--           i(fuzzy)
+--           -- doom_ui_state.query = {
+--           --   type = "settings",
+--           -- }
+--           -- doom_ui_state.next()
+--   		  end
+--       }
+--     }
+--   end
+-- }
+
 ---
 ---@param t_packages
 ---@return list of flattened packages
@@ -440,6 +531,35 @@ end
 -- CONFIGS
 --
 
+-- tree.traverse_table {
+--   tree = doom.settings,
+--   type = "settings",
+--   leaf = function(stack, k, v)
+--     return {
+--       type = "module_config",
+--       data = {
+--         table_path = k,
+--         table_value = v,
+--       },
+--       list_display_props = {
+--         {"CFG"},
+--         {tostring(k)},
+--         {tostring(v)}
+--       },
+--       ordinal = tostring(k),
+--       mappings = {
+--         ["<CR>"] = function(fuzzy,line, cb)
+--           i(fuzzy)
+--           -- doom_ui_state.query = {
+--           --   type = "settings",
+--           -- }
+--           -- doom_ui_state.next()
+--   		  end
+--       }
+--     }
+--   end
+-- }
+
 
 ---
 ---@param t_configs
@@ -480,6 +600,37 @@ end
 --
 -- CMDS
 --
+
+-- tree.traverse_table {
+--   tree = doom.settings,
+--   type = "settings",
+--   leaf = function(stack, k, v)
+--     -- i need to attach k here as well, to table_path
+--     local entry = {
+--       type = "module_cmd",
+--       data = {
+--         name = v[1],
+--         cmd = v[2],
+--       },
+--       ordinal = v[1],
+--       list_display_props = {
+--         {"CMD"},
+--         {tostring(v[1])},
+--         {tostring(v[2])}
+--       },
+--       mappings = {
+--         ["<CR>"] = function(fuzzy,line, cb)
+--           i(fuzzy)
+--           -- doom_ui_state.query = {
+--           --   type = "settings",
+--           -- }
+--           -- doom_ui_state.next()
+--   		  end
+--       }
+--     }
+--   end
+-- }
+
 
 ---
 ---@param t_cmds
