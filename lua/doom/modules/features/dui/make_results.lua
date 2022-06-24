30bdabe2b947a4098795211db2e6fe82c82904d5
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-06-24 03:25:54 -0400

    start moving dui result makers into `utils/tree`

--- lua/doom/modules/features/dui/make_results.lua
+++ lua/doom/modules/features/dui/make_results.lua
@@ -48,7 +48,11 @@ M.get_results_for_query = function(type, components)
     end
 
   elseif doom_ui_state.query.type == "settings" then
-    for _, entry in ipairs(M.settings_flattened(doom.settings)) do
+    for _, entry in ipairs(tree.traverse_table {
+        tree = doom.settings,
+        type = "settings",
+        leaf = mr_settings,
+      }) do
       table.insert(results, entry)
     end
 
@@ -310,10 +314,74 @@ end
 -- SETTINGS (USER/MODULE)
 --
 
--- tree.traverse_table {
---   tree = doom.settings,
---   type = "settings",
---   leaf = function(stack, k, v)
+local function mr_settings(stack, k, v)
+    -- collect table_path back to setting in original table
+    local pc
+    if #stack > 0 then
+      pc = vim.deepcopy(stack)
+      table.insert(pc, k)
+    else
+      pc = { k }
+    end
+    -- REFACTOR: concat table_path
+    -- format each setting
+    local pc_display = table.concat(pc, ".")
+    local v_display
+    if type(v) == "table" then
+      local str = ""
+      for _, x in pairs(v) do
+        if type(x) == "table" then
+          str = str .. ", " .. "subt"
+        else
+          str = str .. ", " .. x
+        end
+      end
+      v_display = str -- table.concat(v, ", ")
+    else
+      v_display = tostring(v)
+    end
+    return {
+      type = "module_setting",
+      data = {
+        table_path = pc,
+        table_value = v,
+      },
+      list_display_props = {
+        {"SETTING"},
+        {pc_display},
+        {v_display}
+      },
+      ordinal = pc_display,
+      mappings = {
+        ["<CR>"] = function(fuzzy,line, cb)
+          i(fuzzy)
+          -- doom_ui_state.query = {
+          --   type = "settings",
+          -- }
+          -- doom_ui_state.next()
+  		  end
+      }
+    }
+end
+
+-- M.settings_flattened = function(t_settings, flattened, stack)
+--   local flattened = flattened or {}
+--   local stack = stack or {}
+--
+--   for k, v in pairs(t_settings) do
+--
+--     if ut.is_sub_setting(k,v) then
+--       -- recurse down
+--       -- if type(k) == "number" then print("!!!!!!!!!!!!") end
+--
+--       -- print("SUB -> ", type(k), k, v)
+--       -- print([[ recurse: (%s:%s), (%s:%s)]], type(k), k, type(v), v)
+--
+--       table.insert(stack, k)
+--       flattened = M.settings_flattened(v, flattened, stack)
+--
+--     else
+--
 --       -- collect table_path back to setting in original table
 --       local pc
 --       if #stack > 0 then
@@ -322,6 +390,7 @@ end
 --       else
 --         pc = { k }
 --       end
+--
 --       -- REFACTOR: concat table_path
 --       -- format each setting
 --       local pc_display = table.concat(pc, ".")
@@ -339,7 +408,8 @@ end
 --       else
 --         v_display = tostring(v)
 --       end
---       return {
+--
+--       local entry = {
 --         type = "module_setting",
 --         data = {
 --           table_path = pc,
@@ -361,145 +431,32 @@ end
 --   		    end
 --         }
 --       }
+--       table.insert(flattened, entry)
+--
+--     end
 --   end
--- }
-
-M.settings_flattened = function(t_settings, flattened, stack)
-  local flattened = flattened or {}
-  local stack = stack or {}
-
-  for k, v in pairs(t_settings) do
-
-    if ut.is_sub_setting(k,v) then
-      -- recurse down
-      -- if type(k) == "number" then print("!!!!!!!!!!!!") end
-
-      -- print("SUB -> ", type(k), k, v)
-      -- print([[ recurse: (%s:%s), (%s:%s)]], type(k), k, type(v), v)
-
-      table.insert(stack, k)
-      flattened = M.settings_flattened(v, flattened, stack)
-
-    else
-
-      -- collect table_path back to setting in original table
-      local pc
-      if #stack > 0 then
-        pc = vim.deepcopy(stack)
-        table.insert(pc, k)
-      else
-        pc = { k }
-      end
-
-      -- REFACTOR: concat table_path
-      -- format each setting
-      local pc_display = table.concat(pc, ".")
-      local v_display
-      if type(v) == "table" then
-        local str = ""
-        for _, x in pairs(v) do
-          if type(x) == "table" then
-            str = str .. ", " .. "subt"
-          else
-            str = str .. ", " .. x
-          end
-        end
-        v_display = str -- table.concat(v, ", ")
-      else
-        v_display = tostring(v)
-      end
-
-      local entry = {
-        type = "module_setting",
-        data = {
-          table_path = pc,
-          table_value = v,
-        },
-        list_display_props = {
-          {"SETTING"},
-          {pc_display},
-          {v_display}
-        },
-        ordinal = pc_display,
-        mappings = {
-          ["<CR>"] = function(fuzzy,line, cb)
-            i(fuzzy)
-            -- doom_ui_state.query = {
-            --   type = "settings",
-            -- }
-            -- doom_ui_state.next()
-  		    end
-        }
-      }
-      table.insert(flattened, entry)
-
-    end
-  end
-
-  table.remove(stack, #stack)
-
-  return flattened
-end
+--
+--   table.remove(stack, #stack)
+--
+--   return flattened
+-- end
 
 --
 -- PACKAGES
 --
 
--- tree.traverse_table {
---   tree = doom.settings,
---   type = "settings",
---   leaf = function(stack, k, v)
---     local spec = v
---     if type(k) == "number" then
---       if type(v) == "string" then
---         spec = { v }
---       end
---     end
---     local repo_name, pkg_name = spec[1]:match("(.*)/([%w%-%.%_]*)$")
---     return {
---       type = "module_package",
---       data = {
---         table_path = k,
---         spec = spec,
---       },
---       list_display_props = {
---         {"PKG"},
---         {repo_name},
---         {pkg_name}
---       },
---       ordinal = repo_name .. pkg_name,
---       mappings = {
---         ["<CR>"] = function(fuzzy,line, cb)
---           i(fuzzy)
---           -- doom_ui_state.query = {
---           --   type = "settings",
---           -- }
---           -- doom_ui_state.next()
---   		  end
---       }
---     }
---   end
--- }
-
----
----@param t_packages
----@return list of flattened packages
-M.packages_flattened = function(t_packages)
-  if t_packages == nil then return end
-  local flattened = {}
-
-  for k, v in pairs(t_packages) do
-
+tree.traverse_table {
+  tree = doom.settings,
+  type = "settings",
+  leaf = function(stack, k, v)
     local spec = v
     if type(k) == "number" then
       if type(v) == "string" then
         spec = { v }
       end
     end
-
     local repo_name, pkg_name = spec[1]:match("(.*)/([%w%-%.%_]*)$")
-
-    local entry = {
+    return {
       type = "module_package",
       data = {
         table_path = k,
@@ -521,12 +478,55 @@ M.packages_flattened = function(t_packages)
   		  end
       }
     }
-
-    table.insert(flattened, entry)
   end
+}
 
-  return flattened
-end
+-- ---
+-- ---@param t_packages
+-- ---@return list of flattened packages
+-- M.packages_flattened = function(t_packages)
+--   if t_packages == nil then return end
+--   local flattened = {}
+--
+--   for k, v in pairs(t_packages) do
+--
+--     local spec = v
+--     if type(k) == "number" then
+--       if type(v) == "string" then
+--         spec = { v }
+--       end
+--     end
+--
+--     local repo_name, pkg_name = spec[1]:match("(.*)/([%w%-%.%_]*)$")
+--
+--     local entry = {
+--       type = "module_package",
+--       data = {
+--         table_path = k,
+--         spec = spec,
+--       },
+--       list_display_props = {
+--         {"PKG"},
+--         {repo_name},
+--         {pkg_name}
+--       },
+--       ordinal = repo_name .. pkg_name,
+--       mappings = {
+--         ["<CR>"] = function(fuzzy,line, cb)
+--           i(fuzzy)
+--           -- doom_ui_state.query = {
+--           --   type = "settings",
+--           -- }
+--           -- doom_ui_state.next()
+--   		  end
+--       }
+--     }
+--
+--     table.insert(flattened, entry)
+--   end
+--
+--   return flattened
+-- end
 
 --
 -- CONFIGS
