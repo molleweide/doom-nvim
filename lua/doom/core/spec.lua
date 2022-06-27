79978548aa4d15b51e88c9f3ff567d2b5ac6d20b
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-06-27 12:09:48 -0400

    core doom spec file

---
+++ lua/doom/core/spec.lua
@@ -0,0 +1,20 @@
+return {
+  module_origins = {
+      "user",
+      "doom",
+  },
+  module_categories = {
+      "core",
+      "features",
+      "langs",
+      "themes"
+  },
+  module_components = {
+      "settings",
+      "packages",
+      "configs",
+      "binds",
+      "cmds",
+      "autocmds",
+  },
+}
