5e36055fda3a5bdc5d83a26a01b3049473e1af2f
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2023-01-02 09:54:45 +0100

    feat(plugins_reloader): wip

--- lua/user/modules/molleweide/nvim_dev/plugins_reloader/init.lua
+++ lua/user/modules/molleweide/nvim_dev/plugins_reloader/init.lua
@@ -1,13 +1,9 @@
-local utils = require("doom.utils")
-local use_floating_win_packer = doom.settings.use_floating_win_packer
 local log = require("doom.utils.logging")
 local system = require("doom.core.system")
 
 -- compare to source git@github.com:notomo/lreload.nvim.git
 
--- TODO: remove / disable
-require("doom.services.commands")
-require("doom.services.autocommands")
+local autocmds_service = require("doom.services.autocommands")
 
 -- TODO: the autocmd also needs to check for lsp errors in each
 --        respective plugin to make sure that we don't reload
@@ -25,7 +21,7 @@ pr.settings = {
     "plugins",
     "syntax",
   },
-  -- watch_ignore_dirs = {}, -- ???
+  watch_ignore_dirs = {},
 }
 local function is_local_path(s)
   return s:match("^~") or s:match("^/")
@@ -33,59 +29,27 @@ end
 
 pr.settings = {}
 
--- -- TODO: return table of all specs
--- local function package_is_local(name, spec)
---   local repo_path = spec[1]
---   if is_local_path(repo_path) then
---     apply_reload_autocmd_to_local_fork(name, repo_path)
---   end
---   if spec.requires ~= nil then
---     for _, rspec in pairs(spec.requires) do
---       local rspec_repo_path = rspec
---       if type(rspec) == "table" then
---         rspec_repo_path = rspec[1]
---       end
---       if is_local_path(rspec_repo_path) then
---         apply_reload_autocmd_to_local_fork(rspec[1]:match("/([_%w%.%-]-)$"), rspec[1])
---       end
---     end
---   end
--- end
-
 local function spawn_autocmds(name, repo_path)
   local repo_lua_path = string.format("%s%slua", repo_path, system.sep)
-  local autocmd_pattern = string.format("%s%s%s", repo_path, system.sep, "**/*.lua")
-  local scan_dir = require("plenary.scandir").scan_dir
-  local reload_module = require("plenary.reload").reload_module
   local scan_dir_opts = { search_pattern = ".", depth = 1, only_dirs = true }
-
-  print("spawn_autocmds")
-
-  -- TODO: use `services.autocommands`
-  utils.make_augroup(name .. "_autoreloader", {
-    {
-      "BufWritePost",
-      autocmd_pattern,
-      function()
-        -- this should be a bool for run on start. not like it is right now.
-        if doom.settings.reload_local_plugins then
-          local t_lua_module_paths = scan_dir(vim.fn.expand(repo_lua_path), scan_dir_opts)
-          local t_lua_module_names = vim.tbl_map(function(s)
-            return s:match("/([_%w]-)$") -- capture only dirname
-          end, t_lua_module_paths)
-          for _, mname in ipairs(t_lua_module_names) do
-            print("reload module name:", mname)
-            -- TODO: log.info("Reloaded package: " .. pname)
-            --    -- if dependency > show `pname (dependency for pmain)`
-            reload_module(mname)
-          end
-        end
-      end,
-    },
+  log.info(string.format([[Spawn watcher for: %s]], repo_path:match("/([_%w%.%-]-)$")))
+  autocmds_service.set({
+    "BufWritePost",
+    string.format("%s%s%s", repo_path, system.sep, "**/*.lua"), -- pattern
+    function()
+      -- if doom.settings.reload_local_plugins then
+      local t_lua_module_paths =
+        require("plenary.scandir").scan_dir(vim.fn.expand(repo_lua_path), scan_dir_opts)
+      local t_lua_module_names = vim.tbl_map(function(s)
+        return s:match("/([_%w]-)$") -- capture only dirname
+      end, t_lua_module_paths)
+      -- for _, mname in ipairs(t_lua_module_names) do
+      log.info("Reloaded package: " .. mname)
+      require("plenary.reload").reload_module(mname)
+      -- end
+      -- end
+    end,
   })
-  log.info(
-    string.format([[Create local reloader autocmd for: %s]], repo_path:match("/([_%w%.%-]-)$"))
-  )
 end
 
 local function find_local_package_paths()
@@ -131,6 +95,7 @@ end
 
 local function remove_package_watchers()
   print("remove_package_watchers")
+  -- TODO: how can I use autocommands service to remove all of them here?
 end
 
 -- if doom.settings.reload_local_plugins then
