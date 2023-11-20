9ac78e90e1341bff7d362eda33a37abeae24e6df
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2023-11-20 08:30:53 -0500

    add email module w himalaya

--- lua/user/modules/molleweide/email/himalaya/init.lua
+++ lua/user/modules/molleweide/email/himalaya/init.lua
@@ -1,4 +1,31 @@
-
 -- cli:   https://github.com/soywod/himalaya
 -- lib:   https://git.sr.ht/~soywod/himalaya-lib
 -- plugin:  https://git.sr.ht/~soywod/himalaya-vim
+
+local email = {}
+
+email.settings = {
+  himalaya = {
+    folder_picker = "telescope", --'native' | 'fzf' | 'telescope'
+    folder_picker_telescope_preview = 1,
+    -- complete_contact_cmd = ""
+
+  },
+}
+
+-- https://github.com/ibhagwan/smartyank.nvim
+email.packages = {
+  ["himalaya-vim"] = {
+    url = "https://git.sr.ht/~soywod/himalaya-vim",
+  },
+}
+
+email.configs = {}
+
+email.configs["himalaya-vim"] = function()
+  vim.g.himalaya_folder_picker = email.settings.himalaya.folder_picker
+  vim.g.himalaya_folder_picker_telescope_preview = email.settings.himalaya.folder_picker_telescope_preview
+  -- vim.g.himalaya_complete_contact_cmd = email.settings.himalaya.complete_contact_cmd
+end
+
+return email
