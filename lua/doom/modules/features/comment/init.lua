a7cdd8ac9e387051056b449548ff494d0e94e6f6
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2024-04-04 04:45:12 -0400

    checking out latest version of comment but `gc` still doesnmt work..

--- lua/doom/modules/features/comment/init.lua
+++ lua/doom/modules/features/comment/init.lua
@@ -1,5 +1,51 @@
 local comment = {}
 
+-- NOTE: DEFAULT CONFIG
+-- {
+--     ---Add a space b/w comment and the line
+--     padding = true,
+--     ---Whether the cursor should stay at its position
+--     sticky = true,
+--     ---Lines to be ignored while (un)comment
+--     ignore = nil,
+--     ---LHS of toggle mappings in NORMAL mode
+--     toggler = {
+--         ---Line-comment toggle keymap
+--         line = 'gcc',
+--         ---Block-comment toggle keymap
+--         block = 'gbc',
+--     },
+--     ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+--     opleader = {
+--         ---Line-comment keymap
+--         line = 'gc',
+--         ---Block-comment keymap
+--         block = 'gb',
+--     },
+--     ---LHS of extra mappings
+--     extra = {
+--         ---Add comment on the line above
+--         above = 'gcO',
+--         ---Add comment on the line below
+--         below = 'gco',
+--         ---Add comment at the end of line
+--         eol = 'gcA',
+--     },
+--     ---Enable keybindings
+--     ---NOTE: If given `false` then the plugin won't create any mappings
+--     mappings = {
+--         ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+--         basic = true,
+--         ---Extra mapping; `gco`, `gcO`, `gcA`
+--         extra = true,
+--     },
+--     ---Function to call before (un)comment
+--     pre_hook = nil,
+--     ---Function to call after (un)comment
+--     post_hook = nil,
+-- }
+
+
 comment.settings = {
   --- Add a space b/w comment and the line
   --- @type boolean
@@ -44,7 +90,7 @@ comment.settings = {
 comment.packages = {
   ["Comment.nvim"] = {
     "numToStr/Comment.nvim",
-    commit = "e89df176e8b38e931b7e71a470f923a317976d86",
+    -- commit = "e89df176e8b38e931b7e71a470f923a317976d86",
   },
 }
 
@@ -67,6 +113,7 @@ comment.binds = {
     "gc",
     [[<cmd>lua require("Comment.api").call('toggle.linewise', '@g')<CR>]],
     name = "Comment motion",
+    expr = true
   },
   {
     "gc",
