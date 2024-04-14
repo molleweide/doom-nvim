018fe2913ec17179aad88dfceef42e0fd413cd5a
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2024-04-14 12:18:23 -0400

    unfreeze and update autopairs

--- lua/doom/modules/features/autopairs/init.lua
+++ lua/doom/modules/features/autopairs/init.lua
@@ -1,9 +1,22 @@
 local autopairs = {}
 
 autopairs.settings = {
+  --     disable_filetype = { "TelescopePrompt", "spectre_panel" }
+  --     disable_in_macro = true  -- disable when recording or executing a macro
+  --     disable_in_visualblock = false -- disable when insert after visual block mode
+  --     disable_in_replace_mode = true
+  --     ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=]
   enable_afterquote = true,
   enable_check_bracket_line = true,
   enable_moveright = true,
+  --     enable_bracket_in_quote = true --
+  --     enable_abbr = false -- trigger abbreviation
+  --     break_undo = true -- switch for basic rule break undo sequence
+  --     check_ts = false
+  --     map_cr = true
+  --     map_bs = true  -- map the <BS> key
+  --     map_c_h = false  -- Map the <C-h> key to delete a pair
+  --     map_c_w = false -- map <c-w> to delete a pair if possible
 }
 
 -- https://github.com/cohama/lexima.vim
@@ -11,7 +24,7 @@ autopairs.settings = {
 autopairs.packages = {
   ["nvim-autopairs"] = {
     "windwp/nvim-autopairs",
-    commit = "f00eb3b766c370cb34fdabc29c760338ba9e4c6c",
+    -- commit = "f00eb3b766c370cb34fdabc29c760338ba9e4c6c",
     event = "InsertEnter",
   },
 }
