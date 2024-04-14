9e614a1c57961e7dfbcd3eb84a6247d59a2d486e
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2024-04-14 02:09:58 -0400

    update intendlines modules w config

--- lua/doom/modules/features/indentlines/init.lua
+++ lua/doom/modules/features/indentlines/init.lua
@@ -1,11 +1,42 @@
 local indentlines = {}
 
 indentlines.settings = {
-  char = "│",
-  use_treesitter = true,
-  show_first_indent_level = false,
-  filetype_exclude = { "help", "dashboard", "packer", "norg", "DoomInfo" },
-  buftype_exclude = { "terminal" },
+  enabled = true,
+  debounce = 300,
+  -- viewport_buffer = ??,
+  indent = {
+    highlight = {
+      "CursorColumn",
+      "Whitespace",
+      --
+      "RainbowRed",
+      "RainbowYellow",
+      "RainbowBlue",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+      "RainbowCyan",
+    },
+    char = "│",
+  },
+  -- whitespace = {
+  --   highlight = { "Function", "Label" },
+  --   -- remove_blankline_trail = true,
+  -- },
+  -- scope = { enabled = true }, -- requires treesitter
+  scope = {
+    enabled = true,
+    show_start = true,
+    show_end = false,
+    injected_languages = false,
+    highlight = { "Function", "Label" },
+    priority = 500,
+  },
+
+  exclude = {
+    filetypes = { "help", "dashboard", "packer", "norg", "DoomInfo" },
+    buftypes = { "terminal" },
+  },
 }
 
 indentlines.packages = {
@@ -13,18 +44,39 @@ indentlines.packages = {
     "lukas-reineke/indent-blankline.nvim",
     -- commit = "c4c203c3e8a595bc333abaf168fcb10c13ed5fb7",
     event = "ColorScheme",
+    -- main = "ibl",
   },
 }
 
 indentlines.configs = {}
 indentlines.configs["indent-blankline.nvim"] = function()
-  require("indent_blankline").setup(
-    vim.tbl_deep_extend("force", doom.features.indentlines.settings, {
-      -- To remove indent lines, remove the module. Having the module and
-      -- disabling it makes no sense.
-      enabled = true,
-    })
-  )
+  -- vim.tbl_deep_extend(
+  --   "force",
+  --   doom.modules.features.indentlines.settings.indent.highlight,
+  --   highlight
+  -- )
+
+  local hooks = require("ibl.hooks")
+  -- create the highlight groups in the highlight setup hook, so they are reset
+  -- every time the colorscheme changes
+  hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+  end)
+
+  require("ibl").setup(doom.modules.features.indentlines.settings)
+  -- require("ibl").setup(
+  --   vim.tbl_deep_extend("force", doom.features.indentlines.settings, {
+  --     -- To remove indent lines, remove the module. Having the module and
+  --     -- disabling it makes no sense.
+  --     enabled = true,
+  --   })
+  -- )
 end
 
 return indentlines
