6153aeb8b558a2c934635aae3799d0b096bec7dc
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2024-07-28 04:31:05 -0400

    whichkey: attempts adding v3 spec keymappings in wk doom module

--- lua/doom/modules/features/whichkey/init.lua
+++ lua/doom/modules/features/whichkey/init.lua
@@ -87,6 +87,8 @@ whichkey.configs["which-key.nvim"] = function()
 
     local keymaps = {}
 
+    local keymaps_v3 = {}
+
     --- Handles each node of the nest keymap config (except the top level)
     --- @param node NestIntegrationNode
     --- @param node_settings NestSettings
@@ -100,20 +102,40 @@ whichkey.configs["which-key.nvim"] = function()
         if keymaps[v] == nil then
           keymaps[v] = {}
         end
+
         -- If this is a keymap group
         if type(node.rhs) == "table" then
           keymaps[v][node.lhs] = { name = node.name }
+          -- v3
+          table.insert(keymaps_v3, {node.lhs, group = node.name})
+
+
           -- If this is an actual keymap
         elseif type(node.rhs) == "string" then
           keymaps[v][node.lhs] = { node.name }
+          -- v3
+          table.insert(keymaps_v3, {node.lhs, node.rhs, mode = v, desc = node.name})
+
         end
+
+        -- build v3 keymap
+
+
       end
+
+
     end
 
     module.on_complete = function()
-      for k, v in pairs(keymaps) do
-        require("which-key").register(v, { mode = k })
-      end
+      print("v3")
+      -- V1 mappings
+      -- P(keymaps_v3)
+      -- for k, v in pairs(keymaps) do
+      --   -- print(k,vim.inspect(v))
+      --   require("which-key").register(v, { mode = k })
+      -- end
+      -- V3 mappings
+      require("which-key").add(keymaps_v3)
     end
 
     return module
